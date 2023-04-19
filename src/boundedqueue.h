88abc47733cb148306a2be90ecb02390c79ef424
Mate Soos <soos.mate@gmail.com>
2023-04-19 16:50:50 -0700

    Adding new option to clear long-term data

--- src/boundedqueue.h
+++ src/boundedqueue.h
@@ -106,6 +106,11 @@ public:
         return longTermAvg;
     }
 
+    void clearLongtTerm()
+    {
+        return longTermAvg.clear();
+    }
+
     T prev(int32_t p) const
     {
         if (p > (int32_t)queuesize)

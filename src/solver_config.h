285b029718fa67ba72fe73b2ce426cc77ca3bbb4
Mate Soos <soos.mate@gmail.com>
2023-04-18 19:07:51 -0700

    Adjustment of parameters

--- src/solver_config.h
+++ src/solver_config.h
@@ -13,7 +13,6 @@ struct CounterConfiguration {
   // TODO comp caching cannot be deactivated for now!
   bool do_non_chron_back_track = true;
   bool do_comp_caching = true;
-  int failed_lit_probe_type = 2;
   double exp = 1.0;
   bool do_pre_processing = true;
   int verb = 1;
@@ -22,11 +21,12 @@ struct CounterConfiguration {
   uint64_t next_restart = 100000000U;
   double restart_cutoff_mult = 0.8;
   uint64_t maximum_cache_size_bytes_ = 0;
-  int restart_type = 5;
+  int restart_type = 4;
   double lookahead_depth = 1.2;
   uint32_t lookahead_num = 2;
+  int failed_lit_probe_type = 2;
   double probe_only_after_ratio = 0.25;
-  double num_probe_multi = 1.0;
+  double num_probe_multi = 1.0; //TODO should try 2.0
   int alluip_inc_act = 1;
   int bprop = 1;
   int do_on_path_print = 0;

652ed1e367f2595b26841d9018e6cadd28120824
Mate Soos <soos.mate@gmail.com>
2023-07-23 20:59:15 +0200

    Extra bump please

--- src/counter_config.hpp
+++ src/counter_config.hpp
@@ -59,7 +59,7 @@ struct CounterConfiguration {
   int do_vivify = 1;
   uint32_t vivif_every = 60000;
   double vivif_mult = 1.0;
-  int do_extra_cl_bump = 0;
+  int do_extra_cl_bump = 1;
   int do_buddy = 0;
   uint32_t buddy_max_cls = 10;
 

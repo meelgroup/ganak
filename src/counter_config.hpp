cc37042f9f46a799bd6716b27ab1ba186c0fbc6e
Mate Soos <soos.mate@gmail.com>
2023-07-25 20:55:35 +0200

    Change default num clauses for buddy

--- src/counter_config.hpp
+++ src/counter_config.hpp
@@ -62,7 +62,7 @@ struct CounterConfiguration {
   double vivif_mult = 1.0;
   int do_extra_cl_bump = 1;
   int do_buddy = 0;
-  uint32_t buddy_max_cls = 10;
+  uint32_t buddy_max_cls = 12;
 
   uint32_t td_varlim = 150000;
   double td_denselim = 0.10;

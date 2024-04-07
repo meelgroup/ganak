55a3839046bfce02054e9d1d258bf581c2db3123
Mate Soos <soos.mate@gmail.com>
2024-04-06 21:57:57 -0400

    Improved graph script

--- scripts/data/create_graphs_ganak.py
+++ scripts/data/create_graphs_ganak.py
@@ -72,7 +72,8 @@ not_versions = ["sharpsat", "gpmc", "6368237b"]
 # exactmc2: out-ganak-6328707.pbs101-7
 # sharpsat: out-ganak-6318929.pbs101-7
 
-only_dirs = ["out-ganak-6396805.pbs101-1/", "6396805.pbs101-12","out-ganak-6318929.pbs101-5", "out-ganak-6328707.pbs101-7", "out-ganak-6318929.pbs101-7"]
+only_dirs = ["6591806", "out-ganak-6396805.pbs101-1/", "6396805.pbs101-12","out-ganak-6318929.pbs101-5", "out-ganak-6328707.pbs101-7", "out-ganak-6318929.pbs101-7"]
+only_dirs = ["6591806"]
 #"6393432", "6393432", "6349002",, "6349002", "6387743" "6356951"] #, "out-ganak-6318929.pbs101-4", "out-ganak-6328707.pbs101-7", "out-ganak-6318929.pbs101-7"] #,"6348728" "6346880", "6335522", "6328982", "6328707"]
 # "6349002",
 not_calls = []

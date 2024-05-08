6ad4ba4628d287e0a25e76e81ed309aa21ed86f9
Mate Soos <soos.mate@gmail.com>
2024-05-08 14:42:25 -0400

    New graph

--- scripts/data/create_graphs_ganak.py
+++ scripts/data/create_graphs_ganak.py
@@ -96,6 +96,7 @@ only_dirs = [
 #             #"out-ganak-6917417.pbs101-", # sortings, lbd, polarity
 #             "out-ganak-6918163.pbs101-0", # restart on mccomp 2023, 20k restarts
              "out-ganak-6965806.pbs101", # mccomp 2022+3, restarts base etc
+             "out-ganak-7015279.pbs101", # new extension and symm run
              ]
 # only_dirs = ["out-ganak-6828273"] #-- functional synth
 #"6393432", "6393432", "6349002",, "6349002", "6387743" "6356951"] #, "out-ganak-6318929.pbs101-4", "out-ganak-6328707.pbs101-7", "out-ganak-6318929.pbs101-7"] #,"6348728" "6346880", "6335522", "6328982", "6328707"]

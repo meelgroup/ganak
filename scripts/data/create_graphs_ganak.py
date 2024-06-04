e571275afdc82983ff2c1cf521c7b1317cf259df
Mate Soos <soos.mate@gmail.com>
2024-06-04 01:04:54 -0400

    Graph

--- scripts/data/create_graphs_ganak.py
+++ scripts/data/create_graphs_ganak.py
@@ -115,13 +115,13 @@ only_dirs = [
              # "out-ganak-7206369.pbs101-",
              # TODO: higher maxw than out-ganak-7205692.pbs101-6, rerun fixed cadiback
              # "out-ganak-7178163.pbs101-0", # proj-2023 16GB, ganak
-             "out-ganak-7178163.pbs101-2", # proj-2023 16GB, d4
-             "out-ganak-7178163.pbs101-3", # proj-2023 16GB, gpmc
+             # "out-ganak-7178163.pbs101-2", # proj-2023 16GB, d4
+             # "out-ganak-7178163.pbs101-3", # proj-2023 16GB, gpmc
              # "out-ganak-7205692.pbs101-6", # old best
              # "out-ganak-7247003.pbs101-", # fixed cadiback, --tdmaxw 40 seems best, but 15 is fine too actually # BEST
-             "out-ganak-7255014.pbs101-0", # different tdminw-s, also try lbd 2. LBD is indifferent, tdminw high is BAD
-             "out-ganak-7266814.pbs101-1", # contract over TDW, higher tdmaxw, higher tdminw. Was buggy in a few ways. Let's re-run. Fixed contraction in the meanwhile.
-             "out-ganak-7294423.pbs101-0", # fixed memout from contraction, fixed too much extend, distill-bin, BIG before backbone
+             # "out-ganak-7255014.pbs101-0", # different tdminw-s, also try lbd 2. LBD is indifferent, tdminw high is BAD
+             # "out-ganak-7266814.pbs101-1", # contract over TDW, higher tdmaxw, higher tdminw. Was buggy in a few ways. Let's re-run. Fixed contraction in the meanwhile.
+             # "out-ganak-7294423.pbs101-0", # fixed memout from contraction, fixed too much extend, distill-bin, BIG before backbone
 
 
              # no point in combining out-ganak-7178422.pbs101-0 with out-ganak-7184237.pbs101-2, 171 either way
@@ -137,9 +137,9 @@ only_dirs = [
              # "out-ganak-7162995.pbs101-0", # new run, good. -- old freq setup
              # "out-ganak-7246958", # fixed cadiback, high tdmaxw
              # "out-ganak-7255018.pbs101-" # tdmaxw 90 + higher tdminw. 12 minw seems to work well
-             # "out-ganak-7255018.pbs101-7", # tdmaxw 90 + higher tdminw. 12 minw seems to work well -- BEST (maxw 90, minw 12)
-             # "out-ganak-7257776.pbs101-9", # higher tdmaxw, higher tdminw -- BEST (maxw 100, minw 15)
-             # "out-ganak-7295009", #fixed memout from contraction, fixed too much extend, distill-bin, BIG before backbone
+             #"out-ganak-7255018.pbs101-7", # tdmaxw 90 + higher tdminw. 12 minw seems to work well -- BEST (maxw 90, minw 12)
+             "out-ganak-7257776.pbs101-9", # higher tdmaxw, higher tdminw -- BEST (maxw 100, minw 15)
+             "out-ganak-7295009.pbs101-0", #fixed memout from contraction, fixed too much extend, distill-bin, BIG before backbone
 
              ]
 # only_dirs = ["out-ganak-6828273"] #-- functional synth
@@ -223,7 +223,7 @@ with open(gnuplotfn, "w") as f:
     f.write("unset logscale y\n")
     f.write("set ylabel  \"Instances counted\"\n")
     f.write("set xlabel \"Time (s)\"\n")
-    f.write("plot [:][20:]\\\n")
+    f.write("plot [:][120:]\\\n")
     i = 0
     # f.write(" \"runkcbox-prearjun.csv.gnuplotdata\" u 2:1 with linespoints  title \"KCBox\",\\\n")
     # f.write(" \"runsharptd-prearjun.csv.gnuplotdata\" u 2:1 with linespoints  title \"SharptTD\",\\\n")

88e4c3d11b4085e2395cec9fcd53df0f731ae416
Mate Soos <soos.mate@gmail.com>
2024-06-05 15:30:25 -0400

    Graph

--- scripts/data/create_graphs_ganak.py
+++ scripts/data/create_graphs_ganak.py
@@ -118,20 +118,22 @@ only_dirs = [
              # "out-ganak-7178163.pbs101-2", # proj-2023 16GB, d4
              # "out-ganak-7178163.pbs101-3", # proj-2023 16GB, gpmc
              # "out-ganak-7205692.pbs101-6", # old best
-             #"out-ganak-7247003.pbs101-", # fixed cadiback, --tdmaxw 40 seems best, but 15 is fine too actually # BEST
+             # #"out-ganak-7247003.pbs101-", # fixed cadiback, --tdmaxw 40 seems best, but 15 is fine too actually # BEST
              # "out-ganak-7255014.pbs101-0", # different tdminw-s, also try lbd 2. LBD is indifferent, tdminw high is BAD
              # "out-ganak-7266814.pbs101-1", # contract over TDW, higher tdmaxw, higher tdminw. Was buggy in a few ways. Let's re-run. Fixed contraction in the meanwhile.
              # "out-ganak-7294423.pbs101-0", # fixed memout from contraction, fixed too much extend, distill-bin, BIG before backbone
-             "out-ganak-7307327.pbs101-4", # fixed cadical, fixed resolv-subs, fixed oracle, 2 new options for extra oracle & resolv-subs
-             "out-ganak-7308235.pbs101-4", # newest cadical -- ALL very good
+             # "out-ganak-7307327.pbs101-4", # fixed cadical, fixed resolv-subs, fixed oracle, 2 new options for extra oracle & resolv-subs
+             #"out-ganak-7308235.pbs101-", # newest cadical -- ALL very good
                                              # best is "--tdmaxw 10 --varfreqdiv 30"
              # BAD PARAMS.... "out-ganak-7316071.pbs101-", # also appmc, play around with some options
+             # "out-ganak-7320764.pbs101-0", #best: --tdmaxw 10 --varfreqdiv 25 , --appmct 2000
+             # "out-ganak-7320764.pbs101-9", #best: --tdmaxw 10 --varfreqdiv 25 , --appmct 2000
 
 
              # no point in combining out-ganak-7178422.pbs101-0 with out-ganak-7184237.pbs101-2, 171 either way
-             # "out-ganak-7178422.pbs101-2", # unproj-2023 16 GB d4
-             # "out-ganak-7178422.pbs101-3", # unproj-2023 16 GB gpmc
-             # "out-ganak-7178422.pbs101-", # unproj-2023 16 GB
+             "out-ganak-7178422.pbs101-2", # unproj-2023 16 GB d4
+             "out-ganak-7178422.pbs101-3", # unproj-2023 16 GB gpmc
+             "out-ganak-7178422.pbs101-", # unproj-2023 16 GB
              # "out-ganak-7180395.pbs101-", #unproj 2023 4GB, freq-128 (vsads)
              # "out-ganak-7184237.pbs101-2", # unproj-2023 4GB, vsads, checking vsads params
              # "out-ganak-7189905.pbs101-" # unproj 2023, 4GB, cache elem simplification, TD back to 0.1, SAT restarts
@@ -141,11 +143,14 @@ only_dirs = [
              # "out-ganak-7162995.pbs101-0", # new run, good. -- old freq setup
              # "out-ganak-7246958", # fixed cadiback, high tdmaxw
              # "out-ganak-7255018.pbs101-7", # tdmaxw 90 + higher tdminw. 12 minw seems to work well -- BEST (maxw 90, minw 12)
-             "out-ganak-7257776.pbs101-9", # higher tdmaxw, higher tdminw -- BEST (maxw 100, minw 15)
-             "out-ganak-7295009.pbs101-0", #fixed memout from contraction, fixed too much extend, distill-bin, BIG before backbone
-             "out-ganak-7306993.pbs101-0",# fixed cadical, fixed resolv-subs, fixed oracle, 2 new options for extra oracle & resolv-subs
+             # "out-ganak-7257776.pbs101-9", # higher tdmaxw, higher tdminw -- BEST (maxw 100, minw 15)
+             #"out-ganak-7295009.pbs101-0", #fixed memout from contraction, fixed too much extend, distill-bin, BIG before backbone
+             # "out-ganak-7306993.pbs101-0",# fixed cadical, fixed resolv-subs, fixed oracle, 2 new options for extra oracle & resolv-subs
                                           # best is "--tdminw 15 --tdmaxw 100 --resolvsub 0 --extraoracle 1"
              # BAD PARAMS "out-ganak-7316065.pbs101", # also appmc, play around with some opitions
+             "out-ganak-7320968.pbs101-1", # best: --tdminw 15 --tdmaxw 100, --appmct 2000
+             "out-ganak-7320968.pbs101-7", # best: --tdminw 15 --tdmaxw 100, --appmct 2000
+
 
              ]
 # only_dirs = ["out-ganak-6828273"] #-- functional synth

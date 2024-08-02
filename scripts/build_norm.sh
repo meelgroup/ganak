80911636560a4056d13fcd7b8e6b80da510a2555
Mate Soos <soos.mate@gmail.com>
2024-08-03 00:39:44 +0200

    Adding tests

--- scripts/build_norm.sh
+++ scripts/build_norm.sh
@@ -1,3 +1,4 @@
-rm -rf CMake* src cmake* ganak* sharp* Make*
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+rm -rf CMake* src cmake* ganak* sharp* Make* Testing* tests*
+cmake -DENABLE_TESTING=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 make -j12
+make test

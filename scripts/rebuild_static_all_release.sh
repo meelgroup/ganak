b9c0a9871451899df14b67dbb6e458ff1c81bfe5
Mate Soos <soos.mate@gmail.com>
2024-08-09 11:50:52 +0200

    Improve build setup

--- scripts/rebuild_static_all_release.sh
+++ scripts/rebuild_static_all_release.sh
@@ -4,41 +4,41 @@ set -e
 cd ../../
 
 cd cadical/
-# git checkout add_dynamic_lib
+git checkout add_dynamic_lib
 make clean
 CXXFLAGS=-fPIC ./configure --competition
 make -j12
 cd ..
 
 cd cadiback/
-# git checkout synthesis
+git checkout synthesis
 make clean
 ./configure
 make -j12
 cd ..
 
 cd breakid/build/
-# git checkout master
+git checkout master
 ./build_static.sh
 cd ../../
 
 cd cryptominisat/build/
-# git checkout synthesis
+git checkout synthesis
 ./build_static_release.sh
 cd ../../
 
 cd sbva/build/
-# git checkout master
+git checkout master
 ./build_static.sh
 cd ../../
 
 cd arjun/build/
-# git checkout synthesis
+git checkout synthesis
 ./build_static_release.sh
 cd ../../
 
 cd approxmc/build/
-# git checkout synthesis
+git checkout synthesis
 ./build_static_release.sh
 cd ../../
 

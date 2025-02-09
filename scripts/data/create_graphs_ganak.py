69abda9c124252cef19173100ebf8ef0a145711a
Mate Soos <soos.mate@gmail.com>
2025-02-09 22:23:19 +0100

    Graph

--- scripts/data/create_graphs_ganak.py
+++ scripts/data/create_graphs_ganak.py
@@ -457,7 +457,10 @@ only_dirs = [
             "out-ganak-mc2324-14145161-", # revert, cache del, TD max limit
 
             # new ccnr ideas, oracle-sparsify timeout different, 0 indep set no puura
-            "out-ganak-mc2324-14164796-",
+            # "out-ganak-mc2324-14164796-",
+
+            # fixed bug in oracle-spars, more options
+            "out-ganak-mc2324-14165187-",
              ]
 # only_dirs = ["out-ganak-6828273"] #-- functional synth
 #"6393432", "6393432", "6349002",, "6349002", "6387743" "6356951"] #, "out-ganak-6318929.pbs101-4", "out-ganak-6328707.pbs101-7", "out-ganak-6318929.pbs101-7"] #,"6348728" "6346880", "6335522", "6328982", "6328707"]

271b9e483b5b83a4156e67d382ee35bc2c101273
Mate Soos <soos.mate@gmail.com>
2025-03-29 17:45:13 +0100

    Update, adding emscripten

--- scripts/build_emscripten.sh
+++ scripts/build_emscripten.sh
@@ -1,4 +1,7 @@
-rm -rf CMake* src cmake* ganak* sharp* Make* Testing* tests* include
-emcmake cmake -DENABLE_TESTING=OFF -DPOLYS=OFF -DCMAKE_INSTALL_PREFIX=$EMINSTALL ..
-emmake make -j12
+#!/bin/bash
+set -e
+
+rm -rf lib* Test* tests* include tests CM* cmake* arjun Makefile rjun-src
+emcmake cmake -DCMAKE_INSTALL_PREFIX=$EMINSTALL -DPOLYS=OFF ..
+emmake make -j26
 emmake make install

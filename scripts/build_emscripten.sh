195847f5b8f60fb23f5577e59743dbceee8b34a8
Mate Soos <soos.mate@gmail.com>
2025-03-30 12:26:18 +0200

    fix emscripten

--- scripts/build_emscripten.sh
+++ scripts/build_emscripten.sh
@@ -2,6 +2,8 @@
 set -e
 
 rm -rf lib* Test* tests* include tests CM* cmake* arjun Makefile rjun-src
-emcmake cmake -DCMAKE_INSTALL_PREFIX=$EMINSTALL -DPOLYS=OFF ..
+emcmake cmake -DCMAKE_INSTALL_PREFIX=$EMINSTALL -DENABLE_TESTING=OFF -DPOLYS=OFF ..
 emmake make -j26
 emmake make install
+cp ganak.wasm ../html
+cp $EMINSTALL/bin/ganak.js ../html

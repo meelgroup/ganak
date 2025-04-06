f485655f517b75d18c585bb840fa6f72a3a2d5b6
vboxuser <vboxuser@vbox.localdomain>
2025-04-06 21:04:44 +0200

    Update

--- scripts/rebuild_dynamic_all.sh
+++ scripts/rebuild_dynamic_all.sh
@@ -1,49 +1,47 @@
 #!/bin/bash
 set -x
-set -e
 cd ../../
 
 cd cadical/
-git checkout add_dynamic_lib
-make clean
+git checkout cav2025
+make -i clean
 CXXFLAGS=-fPIC ./configure --competition
 make -j12
 cd ..
 
 cd cadiback/
-git checkout synthesis
+git checkout cav2025
 make clean
 ./configure
 make -j12
 cd ..
 
 cd breakid/build/
-git checkout master
+git checkout cav2025
 ./build_norm.sh
 cd ../../
 
 cd cryptominisat/build/
-git checkout synthesis
+git checkout cav2025
 ./build_norm.sh
 cd ../../
 
 cd sbva/build/
-git checkout master
+git checkout cav2025
 ./build_norm.sh
 cd ../../
 
 cd arjun/build/
-git checkout synthesis2
+git checkout cav2025
 ./build_norm.sh
 cd ../../
 
 cd approxmc/build/
-git checkout synthesis
+git checkout cav2025
 rm -f build_*.sh
 ln -s ../scripts/build_scripts/build_*.sh .
 ./build_norm.sh
 cd ../../
 
 cd ganak/build/
-# git checkout nodonkey
 ./build_norm.sh

53074d2e7ecd1509928b3682c43de7b755967956
Mate Soos <soos.mate@gmail.com>
2025-06-07 18:59:12 +0200

    Better

--- scripts/mccomp_run_approx.sh
+++ scripts/mccomp_run_approx.sh
@@ -6,13 +6,13 @@ if [ -z "$file" ]; then
 fi
 
 if grep -qE '^c t (mc|pmc)' "$file"; then
-    echo "c o The file seems to be weighted."
+    echo "c o The file seems to be unweighted. Will run with --mode 0."
     ./ganak --mode 0 --maxcache=22000 --appmct 1800 "$file"
 elif grep -qE '^c t (wmc|pwmc)' "$file"; then
-    echo "c o The file seems to be unweighted."
+    echo "c o The file seems to be weighted. Will run with --mode 7."
     ./ganak --mode 7 --maxcache=16000 "$file"
 elif grep -qE '^c t (cpxmc)' "$file"; then
-    echo "c o The file seems to be unweighted."
+    echo "c o The file seems to be complex number-weighted. Will run with --mode 6."
     ./ganak --mode 6 --maxcache=10000 "$file"
 else
   echo "c o The file does not contain a valid mccomp header."
--- scripts/mccomp_run_exact.sh
+++ scripts/mccomp_run_exact.sh
@@ -6,13 +6,13 @@ if [ -z "$file" ]; then
 fi
 
 if grep -qE '^c t (mc|pmc)' "$file"; then
-    echo "c o The file seems to be weighted."
+    echo "c o The file seems to be unweighted. Will run with --mode 0."
     ./ganak --mode 0 --maxcache=22000 "$file"
 elif grep -qE '^c t (wmc|pwmc)' "$file"; then
-    echo "c o The file seems to be unweighted."
+    echo "c o The file seems to be weighted. Will run with --mode 1."
     ./ganak --mode 1 --maxcache=16000 "$file"
 elif grep -qE '^c t (cpxmc)' "$file"; then
-    echo "c o The file seems to be unweighted."
+    echo "c o The file seems to be complex number-weighted. Will run with --mode 2."
     ./ganak --mode 2 --maxcache=10000 "$file"
 else
   echo "c o The file does not contain a valid mccomp header."

69f7041d60ff79e4c69135e0cce106f56b51c3b5
Mate Soos <mate.soos@argot.org>
2026-03-28 15:13:26 +0000

    Fixing build scripts

--- scripts/build_debug.sh
+++ scripts/build_debug.sh
@@ -2,4 +2,4 @@
 
 rm -rf make* CMake* src cmake* ganak* sharp* Make*
 cmake -DCMAKE_BUILD_TYPE=Debug ..
-make -j12 VERBOSE=1
+make -j$(nproc) VERBOSE=1
--- scripts/build_norm.sh
+++ scripts/build_norm.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 rm -rf CMake* src cmake* ganak* sharp* Make* Testing* tests* include .cmake
 cmake -DENABLE_TESTING=ON ..
-make -j12
+make -j$(nproc)
 make test

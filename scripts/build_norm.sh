93da74b7ef8782dc5dd34f4dad9a6cd12d019302
Mate Soos <soos.mate@gmail.com>
2026-04-04 16:43:14 +0200

    Fixing build script

--- scripts/build_norm.sh
+++ scripts/build_norm.sh
@@ -1,28 +1,29 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
 
-rm -rf CMake*
-rm -rf src
-rm -rf cmake*
-rm -rf ganak*
-rm -rf sharp*
-rm -rf Make*
-rm -rf Testing*
+rm -rf .cmake
+rm -rf lib*
+rm -rf Test*
 rm -rf tests*
 rm -rf include
-rm -rf .cmake
+rm -rf tests
+rm -rf approxmc*
+rm -rf apx-src
+rm -rf CM*
+rm -rf cmake*
 rm -rf deps
 rm -rf _deps
-SAT_DIR=$(cd ../.. && pwd)
+SOLVERS_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
+echo "solvers dir: $SOLVERS_DIR"
 cmake -DENABLE_TESTING=ON \
-    -Dcryptominisat5_DIR="${SAT_DIR}/cryptominisat/build" \
-    -Dsbva_DIR="${SAT_DIR}/sbva/build" \
-    -Dtreedecomp_DIR="${SAT_DIR}/treedecomp/build" \
-    -DEvalMaxSAT_DIR="${SAT_DIR}/EvalMaxSAT/build" \
-    -Darjun_DIR="${SAT_DIR}/arjun/build" \
-    -Dapproxmc_DIR="${SAT_DIR}/approxmc/build" \
-    -Dcadical_DIR="${SAT_DIR}/cadical/build" \
-    -Dcadiback_DIR="${SAT_DIR}/cadiback/build" \
+    -Dcadical_DIR="${SOLVERS_DIR}/cadical/build" \
+    -Dcadiback_DIR="${SOLVERS_DIR}/cadiback/build" \
+    -Dcryptominisat5_DIR="${SOLVERS_DIR}/cryptominisat/build" \
+    -Dsbva_DIR="${SOLVERS_DIR}/sbva/build" \
+    -Dtreedecomp_DIR="${SOLVERS_DIR}/treedecomp/build" \
+    -Darjun_DIR="${SOLVERS_DIR}/arjun/build" \
+    -Dapproxmc_DIR="${SOLVERS_DIR}/approxmc/build" \
     ..
 make -j$(nproc)
 make test

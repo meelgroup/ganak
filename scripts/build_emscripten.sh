eee8a2b46cc6bf8d581140b0c24f9ef600cabbf4
Mate Soos <soos.mate@gmail.com>
2026-04-15 11:03:45 +0200

    Fix emscripten

--- scripts/build_emscripten.sh
+++ scripts/build_emscripten.sh
@@ -16,7 +16,7 @@ rm -rf _deps
 SAT_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
 emcmake cmake -DCMAKE_INSTALL_PREFIX=$EMINSTALL -DENABLE_TESTING=OFF \
     -Dcadical_DIR="${SAT_DIR}/cadical/build" \
-    -Dcadiback_DIR="${SAT_DIR}/cadiback" \
+    -Dcadiback_DIR="${SAT_DIR}/cadiback/build" \
     -Dcryptominisat5_DIR="${SAT_DIR}/cryptominisat/build" \
     -Dsbva_DIR="${SAT_DIR}/sbva/build" \
     -Dtreedecomp_DIR="${SAT_DIR}/treedecomp/build" \

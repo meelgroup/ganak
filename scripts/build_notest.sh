977c05c865d43dcbe212a9fec55f20be452ab373
Mate Soos <soos.mate@gmail.com>
2026-05-24 10:26:47 +0200

    Build test script

---
+++ scripts/build_notest.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+rm -rf .cmake
+rm -rf lib*
+rm -rf Test*
+rm -rf tests*
+rm -rf include
+rm -rf tests
+rm -rf approxmc*
+rm -rf apx-src
+rm -rf CM*
+rm -rf cmake*
+rm -rf deps
+rm -rf _deps
+SAT_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
+echo "solvers dir: $SAT_DIR"
+cmake -DENABLE_TESTING=OFF \
+    -Dcadical_DIR="${SAT_DIR}/cadical/build" \
+    -Dcadiback_DIR="${SAT_DIR}/cadiback/build" \
+    -Dcryptominisat5_DIR="${SAT_DIR}/cryptominisat/build" \
+    -Dsbva_DIR="${SAT_DIR}/sbva/build" \
+    -Dtreedecomp_DIR="${SAT_DIR}/treedecomp/build" \
+    -Darjun_DIR="${SAT_DIR}/arjun/build" \
+    -Dapproxmc_DIR="${SAT_DIR}/approxmc/build" \
+    ..
+make -j$(nproc)

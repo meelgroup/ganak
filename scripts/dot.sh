92b58988104e75c70e1165bf8e9b9d78326c4068
Mate Soos <soos.mate@gmail.com>
2026-06-08 11:34:29 +0200

    Update dot.sh

--- scripts/dot.sh
+++ scripts/dot.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+set -e
+set -x
 ./ganak --compile my.ddnnf a.cnf
-./ddnf-cleanup my.ddnnf my-cleaned.ddnnf
+./ddnnf-cleanup my.ddnnf my-cleaned.ddnnf
 ./ddnnf2dot my-cleaned.ddnnf my.dot
 dot -Tpdf my.dot -o my.pdf
 okular my.pdf
-

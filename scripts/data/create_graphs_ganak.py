dcd478024932c1103535966e512566da17c971f9
Mate Soos <soos.mate@gmail.com>
2026-06-16 16:39:16 +0200

    MCC check Graph

--- scripts/data/create_graphs_ganak.py
+++ scripts/data/create_graphs_ganak.py
@@ -2028,11 +2028,11 @@ only_dirs = [
     # "out-ganak-mccomp2324-1517017-0", # 4 full runs for all the 4 new arjun orders
     # "out-ganak-mccomp2324-1635700-0", # fix the printing of the preproc data
     # "out-ganak-mccomp2324-1743408", # ddnnf
-    # "out-ganak-mccomp2324-1747186-0", # faster ddnnf, new hash function
-    "out-ganak-mccomp2324-1755057-0", # 5 min timeout
-    "out-ganak-mccomp2324-1755057-3", # 5 min timeout
-    "out-ganak-mccomp2324-1758343-5", # new 5 min timeout run
-    "out-ganak-mccomp2324-1762059-", # new 5 min timeout run
+    "out-ganak-mccomp2324-1747186-0", # faster ddnnf, new hash function
+    # "out-ganak-mccomp2324-1755057-0", # 5 min timeout
+    # "out-ganak-mccomp2324-1755057-3", # 5 min timeout
+    # "out-ganak-mccomp2324-1758343-5", # new 5 min timeout run
+    # "out-ganak-mccomp2324-1762059-", # new 5 min timeout run
 ]
 # only_dirs = [
 #      "mei-march-2026-1239767-1", # gpmc
